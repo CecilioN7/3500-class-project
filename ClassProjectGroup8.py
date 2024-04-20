ea6cfaf9b8dc643f209f1f0c740055d2ceea0f9b
CecilioN7 <navarroscecilio@gmail.com>
2024-04-19 17:33:34 -0700

    main class project file

---
+++ ClassProjectGroup8.py
@@ -0,0 +1,9 @@
+# course: cmps3500
+# CLASS Project
+# PYTHON IMPLEMENTATION: BASIC DATA ANALYSIS
+# date: 04/19/24
+# Student 1: Cecilio Navarro
+# Student 2: 
+# Student 3: 
+# Student 4: 
+# description: Implementation Basic Data Analysis Routines
